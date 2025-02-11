1ef239e79810d92a53a0ada3699096cc66487c08
Mahmoud Barzegar <m.bardehgar.hedeshi@gmail.com>
2025-02-11 23:42:10 +0330

    whenQueryingForLongerThan added for loging slow query.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::share('site_name', config('app.name'));
+
+        DB::enableQueryLog();
+        DB::whenQueryingForLongerThan(10, function ($connection, $query) {
+            Log::warning('Slow query details:', [
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+                'execution_time_ms' => $query->time,
+                'connection' => $connection->getName(),
+                'database' => $connection->getDatabaseName(),
+                'url' => request()->url(),
+                // 'user_id' => auth()->id,
+                'timestamp' => now()->toDateTimeString(),
+                // 'query_log' => $connection->getQueryLog()
+            ]);
+        });
+
+        DB::disableQueryLog();
     }
 }
